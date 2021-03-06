88842394abe52941337ed294470b7c98ab953801
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-03-06 16:48:14 +0100

    Add Navbar elements + Trim Example code

--- Transcom.Web/Startup.cs
+++ Transcom.Web/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Transcom.Web.Data;
+
+
 
 namespace Transcom.Web
 {
@@ -28,7 +23,6 @@ namespace Transcom.Web
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

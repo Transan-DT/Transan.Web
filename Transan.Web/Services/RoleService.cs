3a09d9634a38d98965226458d7be29850732f228
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-02-19 16:09:26 +0100

    Draft Role Service + DTOs

---
+++ Transan.Web/Services/RoleService.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Transan.Web.Data.RoleDeck;
+
+namespace Transan.Web.Services;
+
+public class RoleService
+{
+	private readonly ILogger _logger;
+	private readonly IConfiguration _config;
+	private readonly DiscordClient _discordClient;
+	private readonly IMongoCollection<RoleCategory> _roles;
+
+	public RoleService(ILogger<RoleService> logger, IMongoClient mongoClient, IConfiguration config, DiscordClient discordClient)
+	{
+		_logger = logger;
+		_config = config;
+		_discordClient = discordClient;
+		_roles = mongoClient.GetDatabase(config["MongoDb:Databases:Web"]).GetCollection<RoleCategory>("Roles");
+	}
+
+	public async Task<IEnumerable<RoleCategory>> GetRoleDeckcategoriesAsync() => 
+		await (await _roles.FindAsync(FilterDefinition<RoleCategory>.Empty, 
+			new() { Sort = new SortDefinitionBuilder<RoleCategory>().Descending(r => r.Order) }))
+			.ToListAsync();
+
+	public async Task<IEnumerable<DiscordRole>> GetAllServerRolesAsync(bool includeSeparatorRoles)
+	{
+		DiscordGuild guild = await _discordClient.GetGuildAsync(_config.GetValue<ulong>("DiscordIntegration:Server:Id"));
+		return includeSeparatorRoles
+			? guild.Roles.Values
+			: guild.Roles.Values.Where(r => !r.Name.StartsWith('.'));
+	}
+	
+	public async Task<IEnumerable<ulong>> GetMemberRolesAsync(ulong userId)
+	{
+		DiscordGuild guild = await _discordClient.GetGuildAsync(_config.GetValue<ulong>("DiscordIntegration:Server:Id"));
+		return (await guild.GetMemberAsync(userId)).Roles.Select(r => r.Id);
+	}
+
+	public async Task SetMemberRoleAsync(RoleChangeDto roleChange)
+	{
+		DiscordGuild guild = await _discordClient.GetGuildAsync(_config.GetValue<ulong>("DiscordIntegration:Server:Id"));
+		DiscordMember member = await guild.GetMemberAsync(roleChange.UserId);
+		DiscordRole role = guild.GetRole(roleChange.RoleId);
+
+		// Just a routine security check
+		if (role.Permissions is not Permissions.None) // Oh? Got some perms?
+		{
+			// Check if role is truly and well listed in DB, as assignable.
+			if (!await (await _roles.FindAsync(new FilterDefinitionBuilder<RoleCategory>().ElemMatch(r => r.Roles, r => r.Id == role.Id))).AnyAsync())
+			{
+				// This could've been dangerous. Imagine having assigned Admin perms to some random user in screening...
+				throw new SecurityException("Attempted to assign an unlisted role with permissions attached. This has been denied as it constitutes a security risk.");
+			}
+		}
+		
+		const string reasonMessage = "[transanctuaire.fr] Assigned from user's Role Deck.";
+
+		if (roleChange.State)
+		{
+			await member.GrantRoleAsync(role, reasonMessage);
+		}
+		else
+		{
+			await member.RevokeRoleAsync(role, reasonMessage);
+		}
+	}
+}
